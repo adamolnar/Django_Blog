4523a69796f16052bfc9f23baf42552316c3fbd8
adamolnar <adamolnar121@gmail.com>
2023-10-26 09:05:03 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -28,10 +29,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORGIN'
 
 ALLOWED_HOSTS = ['django-blog123-7cca18c256ad.herokuapp.com', 'localhost']
-ALLOWED_HOSTS = ['8000-adamolnar-djangoblog-mv0otwp8bny.ws-eu105.gitpod.io']
 
 
 # Application definition
@@ -61,6 +63,14 @@ LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
